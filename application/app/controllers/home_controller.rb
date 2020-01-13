6a231fe61ce8e75e7022ec096979c0dafa6a8854
Masaki Ikegami <salvage0707@gmail.com>
2020-01-13 13:28:26 +0900

    update project

--- application/app/controllers/home_controller.rb
+++ application/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    render json: { message:"Application Message OK" }
+    render json: { message:"Application Message deploy OK" }
   end
 end
