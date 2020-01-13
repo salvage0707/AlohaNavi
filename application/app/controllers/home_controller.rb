9eafe53288c4b66abfaec85efb5566bb2e7d22b7
Masaki Ikegami <salvage0707@gmail.com>
2020-01-13 16:06:33 +0900

    CI/CD test

--- application/app/controllers/home_controller.rb
+++ application/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    render json: { message:"Application Message deploy OK" }
+    render json: { message:"Application Message deploy rails_api_template OK" }
   end
 end
